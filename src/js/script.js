0afc4a9d09ccada4756568a202d47bfb5a8e0da9
rmzsmyl <rmzsmyl@gmail.com>
2023-09-21 21:46:27 +0400

    update index3 html

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,22 @@
 class User {
-  constructor(ad, soyad, password) {
-      this.ad = ad;
-      this.soyad = soyad;
-      this.password = password;
-  }
+    constructor(ad, soyad, password) {
+        this.ad = ad;
+        this.soyad = soyad;
+        this.password = password;
+    }
 
-  fullname() {
-      return this.ad + ' ' + this.soyad;
-  }
-  
-  showFullnameInUpperCase() {
-      const fullName = this.fullname().toUpperCase();
-      document.getElementById('userFullName').textContent = fullName;
-  }
+    fullname() {
+        return `${this.ad} ${this.soyad}`;
+    }
+
+    toHtml() {
+        const fullname = this.fullname().toUpperCase();
+        return fullname; 
+    }
 }
 
-const user = new User('John', 'Doe', 'password123');
 
-user.showFullnameInUpperCase();
+const user = new User("John", "Doe", "password123");
+const fullname = user.toHtml();
+const userFullName = document.getElementById("userFullName");
+userFullName.textContent = fullname; 
